b37f3b944d65de0174543a1a5196a7973d29a20b
MINHDQHe187308 <minhduongqh0311@gmail.com>
2026-03-22 09:04:54 +0700

    fIX warning Text strings must be rendered within a <Text> component.

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { Dimensions, Image, Modal, SafeAreaView, ScrollView, TouchableOpacity, View } from 'react-native';
+import { Dimensions, Image, Modal, ScrollView, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
